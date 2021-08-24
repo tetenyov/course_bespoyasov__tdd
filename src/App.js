6656bce157cd44e3c1b2f72a1695ee6838c34659
vovo <tetenev@yandex.ru>
2021-08-24 08:23:47 +0300

    blueprint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import styles from "./App.module.css";
 export function App() {
   return (
     <main className={styles.main}>
-
+      {/* Header */}
+      {/* Converter form */}
+      {/* Footer */}
     </main>
   );
 }
+
