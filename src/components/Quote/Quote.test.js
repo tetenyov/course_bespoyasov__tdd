f80848bcbddd1ff52bb297dfecfa450192eb95cb
vovo <tetenev@yandex.ru>
2021-08-24 08:57:01 +0300

    Render a quote text

--- src/components/Quote/Quote.test.js
+++ src/components/Quote/Quote.test.js
@@ -1,12 +1,15 @@
 import { render, screen } from "@testing-library/react"
 import { Quote } from "./Quote";
+import { Quotes } from './quotes';
 
 describe('when rendered', () => {  //from jest
+  const testQuote = Quotes[0];
+
   it('should contain an expected text', () => {  // from jest
     render(<Quote />);  //from react testing library
     // screen позволяет вз д с тем куском дерева, который рендерит render
 
-    const result = screen.getByText(/Test Quote/i);
+    const result = screen.getByText(testQuote.text);
     expect(result).toBeInTheDocument() //если найдешь result, то он должен быть внутри Document
-  })
-})
+  });
+});
