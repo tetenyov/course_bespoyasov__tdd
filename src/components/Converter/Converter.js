3f13ec637e4633f3935f32b307af615de1303d1c
vovo <tetenev@yandex.ru>
2021-08-26 10:15:25 +0300

    Use control inputs

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,6 +1,10 @@
+import React, { useState } from 'react';
 import styles from './converter.module.css';
 
 export function Converter() {
+  const [rub, setRub] = useState(100);
+  const [usd, setUsd] = useState(1);
+
   return (
     <form className={styles.converter}>
       <label>
@@ -10,6 +14,8 @@ export function Converter() {
         <input 
           type='number'
           name='rub'
+          value={rub}
+          onChange={(evt) => setRub(evt.target.value)}
           min='0'
           step='1'
         />
@@ -21,6 +27,8 @@ export function Converter() {
         <input 
           type='number'
           name='usd'
+          value={usd}
+          onChange={(evt) => setUsd(evt.target.value)}
           min='0'
           step='1'
         />
--- src/components/Converter/Converter.test.js
+++ src/components/Converter/Converter.test.js
@@ -1,12 +1,41 @@
 import { render, screen } from "@testing-library/react";
 import { Converter } from './Converter';
-
+import userEvent from '@testing-library/user-event';
 
 describe('when rendered', () => {
   it('rub input should have a value with a rub amount', () => {
     render(<Converter />);
 
     expect(screen.getByLabelText(/Сумма в рублях/))
+      .toHaveValue(100);
+  })
+
+  it('usd input should have a value with a usd amount', () => {
+    render(<Converter />);
+
+    expect(screen.getByLabelText(/Сумма в долларах/))
       .toHaveValue(1);
+  })
+})
+
+describe('when typed in a RUB input', () => {
+  it('should update its value', () => {
+    render(<Converter />);
+    const input = screen.getByLabelText(/Сумма в рублях/);
+    
+    userEvent.clear(input);
+    userEvent.type(input, '42'); //когда юзер пишет внутри инпута 42
+    expect(input).toHaveValue(42);
+  })
+})
+
+describe('when typed in a USD input', () => {
+  it('should update its value', () => {
+    render(<Converter />);
+    const input = screen.getByLabelText(/Сумма в долларах/);
+    
+    userEvent.clear(input);
+    userEvent.type(input, '42'); //когда юзер пишет внутри инпута 42
+    expect(input).toHaveValue(42);
   })
 })
