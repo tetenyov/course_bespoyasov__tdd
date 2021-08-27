20c6c2490568454b566c089e518f8988c4d65806
vovo <tetenev@yandex.ru>
2021-08-27 08:49:17 +0300

    Plan test for the hook

---
+++ src/components/Converter/useConverter.test.js
@@ -0,0 +1,18 @@
+describe('when rendered', () => {
+  it.todo('rub input should have a value equal to initial rub amount'); // планируем тест (todo)
+  it.todo('usd input should have a value equal to initial usd amount');
+});
+
+describe('when call an updateRub method', () => {
+  it.todo('should update rub value');
+  it.todo('should recalculate usd value');
+})
+
+describe('when call an updateUSD method', () => {
+  it.todo('should update usd value');
+  it.todo('should recalculate rub value');
+})
+
+describe('when re-rendered', () => {
+  it.todo('should update its values');
+})
