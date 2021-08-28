99bbd6cb394b0217743f06c3df154e04237ce096
Vovo <tetenev@yandex.ru>
2021-08-28 14:37:29 +0300

    Wrap the App in the store provider

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { CurrentCourse } from '../CurrenCourse/CurrentCourse';
 import styles from './header.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
       Курс доллара на сегодня
+      <CurrentCourse />
     </header>
   )
 }
