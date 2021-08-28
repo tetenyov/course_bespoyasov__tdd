0303c72ad9ba06bc61128018df6f2997598421dd
Vovo <tetenev@yandex.ru>
2021-08-28 16:11:23 +0300

    Add an async thunk

--- src/store/features/course/slice.js
+++ src/store/features/course/slice.js
@@ -1,12 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchCourse } from '../../../api';
+
+
 
 export const initialCourseState = {
   value: 42,
+  status: 'idle',
+  error: null,
 };
+
+export const fetchTodayCourse = createAsyncThunk(
+  "course/fetchCourse",
+  async () => {
+    const response = await fetchCourse;
+    return response.course;
+  }
+)
 
 const courseSlice = createSlice({
   name: 'course',
   initialState: initialCourseState,
+  extraReducers: {
+    [fetchTodayCourse.pending]: () => {},
+    [fetchTodayCourse.fulfilled]: () => {},
+    [fetchTodayCourse.rejected]: () => {},
+  }
 });
 
 export const selectCourse = (state) => state.course.value;
