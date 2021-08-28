74375315af6dd83a705741454b35b5475d43c54c
Vovo <tetenev@yandex.ru>
2021-08-28 12:55:31 +0300

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '.header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      Курс доллара на сегодня
+    </header>
+  )
+}
+
