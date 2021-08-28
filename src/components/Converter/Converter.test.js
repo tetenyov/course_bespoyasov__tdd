31f952904534f6ac13042c6b31cf0a28d476832b
Vovo <tetenev@yandex.ru>
2021-08-28 12:21:40 +0300

    Use mocks to test the component connection to hook

--- src/components/Converter/Converter.test.js
+++ src/components/Converter/Converter.test.js
@@ -2,7 +2,22 @@ import { render, screen } from "@testing-library/react";
 import { Converter } from './Converter';
 import userEvent from '@testing-library/user-event';
 
+const mockUpdateRub = jest.fn();//fn - метод, который создает функцию, которая следит за тем, что с ней происходит
+const mockUpdateUsd = jest.fn();
+
+jest.mock('./useConverter', () => ({
+  useConverter() {
+    return {
+      rub: 100,
+      usd: 2.38,
+      updateRub: mockUpdateRub,
+      updateUsd: mockUpdateUsd,
+    }
+  }
+}))
+
 describe('when rendered', () => {
+  // it.only('rub input should have a value with a rub amount', () => { //only запускает только этот конкретный тест. позволяет определить какой именно тест не проходит
   it('rub input should have a value with a rub amount', () => {
     render(<Converter />);
 
@@ -25,7 +40,7 @@ describe('when typed in a RUB input', () => {
     
     userEvent.clear(input);
     userEvent.type(input, '42'); //когда юзер пишет внутри инпута 42
-    expect(input).toHaveValue(42);
+    expect(mockUpdateRub).toHaveBeenCalledWith('42');
   })
 })
 
@@ -36,6 +51,6 @@ describe('when typed in a USD input', () => {
     
     userEvent.clear(input);
     userEvent.type(input, '42'); //когда юзер пишет внутри инпута 42
-    expect(input).toHaveValue(42);
+    expect(mockUpdateUsd).toHaveBeenCalledWith('42');
   })
 })
